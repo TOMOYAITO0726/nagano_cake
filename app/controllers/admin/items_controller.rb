993d76aa6680a6d3fdd0e1cb9ee5d21fd428c518
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2022-07-24 13:19:14 +0000

    [start]item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,29 @@
 class Admin::ItemsController < ApplicationController
+  
+  def new
+   @item=Item.new
+  end  
+  
+  def create
+   @item=Item.new(item_params)
+   @item.save
+    redirect_to admin_item_path(@item.id)
+  end  
+  
+  def show
+    @item=Item.find(params[:id])
+  end 
+  
+  def edit
+  end
+  
+  def update
+  end  
+  
+  private
+  def item_params
+    params.require(:item).permit(:name,:introduction,:price,:is_active,:image, :genre_id)
+  end  
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
    namespace :admin do
-   get 'items' => 'admins/items#index'
+   #get 'items' => 'admins/items#index'
    get 'homes' => 'homes#top'
  end 
    get 'items' => 'customers/items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  namespace :admin do
    resources :genres, only: [:index, :create, :edit, :update]
+   resources :items, only: [:index, :new, :create, :show, :edit, :update]
  end   
  
  devise_for :admin, controllers: {
