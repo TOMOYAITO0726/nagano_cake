298125f005fc237baedf49ee83e7925e1ee145a3
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2022-07-31 12:49:07 +0000

    [fix]address index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,38 @@
 class Public::AddressesController < ApplicationController
 
+ def new
+   @address=Address.new
+ end
+ 
  def index
+   @addresses=Address.all
+   @address=Address.new
  end
  
  def edit
+  @address=Address.find(params[:id])
  end
  
  def create
+  @address=Address.new(address_params)
+  @address.customer_id=current_customer.id
+  @address.save
+  redirect_to  public_addresses_path
  end
  
  def update
+  @address=Address.find(params[:id])
+  @address.update(address_params)
+  redirect_to  public_addresses_path
  end
  
  def destroy
  end
  
+ private
+ 
+ def address_params
+  params.require(:address).permit(:name,:postal_code,:address) 
+ end   
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
    get '/customers/edit'=>'customers#edit'
    patch 'customers/update'=>'customers#update'
    get '/customers/quit'=>'customers#quit'
+   resources :addresses, only: [:index, :create, :new, :edit, :update, :destory]
    end
    #get 'items' => 'customers/items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
